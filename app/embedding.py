e2008adc1613e75fa5750a4e73e1e00f954a94c6
Piyush <167032565+desciphear@users.noreply.github.com>
2025-07-27 22:13:12 +0530

    Update embedding.py

--- app/embedding.py
+++ app/embedding.py
@@ -1,8 +1,8 @@
-import fasttext
+from sentence_transformers import SentenceTransformer
 
 class Embedder:
-    def __init__(self, model_path="cc.en.300.bin"):
-        self.model = fasttext.load_model(model_path)
-    
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        self.model = SentenceTransformer(model_name, device="cpu")
+
     def embed_texts(self, texts: list[str]) -> list[list[float]]:
-        return [self.model.get_sentence_vector(text).tolist() for text in texts]
+        return self.model.encode(texts, show_progress_bar=True, convert_to_numpy=True).tolist()
